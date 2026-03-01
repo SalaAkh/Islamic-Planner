065c96e16a3d88ac3c2573b5e006184531c5271a
Akhmedyanov Salamat <vffatop@gmail.com>
2026-03-01 13:05:27 +0500

    config: Update Firebase API key and add Firestore indexes configuration.

--- firebase-init.js
+++ firebase-init.js
@@ -4,7 +4,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase
 
 // User's actual Firebase Config
 const firebaseConfig = {
-    apiKey: "AIzaSyAo7qZrXlVmx53rhmoyLgbJccVL7EMtNoU",
+    apiKey: "AIzaSyAo7qZrXlVMx53rhmoyLgbJccVL7EMtNoU",
     authDomain: "islamic-planer.firebaseapp.com",
     projectId: "islamic-planer",
     storageBucket: "islamic-planer.firebasestorage.app",
